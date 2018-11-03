026b8d8e279ab0c081271b1b9588474e65a95213
Александр Дрожкин <Santay2009@yandex.ru>
2018-11-04 02:21:07 +0300

    Поставил анимацию

--- НОВОЕДЛЯДЕСТОПА/App.xaml.cs
+++ НОВОЕДЛЯДЕСТОПА/App.xaml.cs
@@ -13,5 +13,6 @@ namespace НОВОЕДЛЯДЕСТОПА
     /// </summary>
     public partial class App : Application
     {
+
     }
 }
--- НОВОЕДЛЯДЕСТОПА/MainWindow.xaml.cs
+++ НОВОЕДЛЯДЕСТОПА/MainWindow.xaml.cs
@@ -5,10 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,30 +26,6 @@ namespace НОВОЕДЛЯДЕСТОПА
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
-
-        private void Login_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Получили сообщение для входа в систему!");
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            
-        }
-
-        private void TextBox_KeyDown(object sender, KeyEventArgs e)
-        {
-            if ((char)e.Key == (char)Key.Enter)
-            {
-                TextBox text = (TextBox)sender;
-                MessageBox.Show(text.Text);
-            }
         }
     }
 }
