680b81c36548f5c34ab9816fe62ce65762431b9d
Александр Дрожкин <Santay2009@yandex.ru>
2018-11-04 00:26:48 +0300

    Программа для просмотра

---
+++ НОВОЕДЛЯДЕСТОПА/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace НОВОЕДЛЯДЕСТОПА
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Получили сообщение для входа в систему!");
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((char)e.Key == (char)Key.Enter)
+            {
+                TextBox text = (TextBox)sender;
+                MessageBox.Show(text.Text);
+            }
+        }
+    }
+}
